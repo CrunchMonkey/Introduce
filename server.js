f5a476a20aca149f8cc2f408c72da0aa825ab174
이승렬 <dltmdfuf95@naver.com>
2023-01-16 09:55:21 +0900

    db.js삭제 소개페이지 마무리

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const db = require('./db');
-const conn = db.conn;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname  + "/views"); //__dirname 은 현재폴더
@@ -13,19 +11,7 @@ app.use('/uploads', express.static('uploads'));
 //소개 + 사진
 
 app.get('/', function (req, res) {
-  //res.send('Hello World');
-  // conn.query("SELECT 1 as val", (err, rows) => {
-  //     console.log(rows); //[ {val: 1}, meta: ... ]
-  // });
-  var skill;
-  conn.query("SELECT * FROM skill_table", (err, res) => {
-    //console.log(res[1]);
-    skill = res;
-    //conn.end();
-  });
-  console.log(skill);
-  res.render("main", skill);
+  res.render("main");
 })
-
 
 app.listen(3000)
